5ca5595e9df9a0c3a1ef3c0aa93569b3eb0d4612
Moufid Larbi <moufidlarbi@gmail.com>
2017-04-24 19:22:08 -0400

    Added button to make tasks private

--- imports/ui/Task.jsx
+++ imports/ui/Task.jsx
@@ -13,6 +13,10 @@ export default class Task extends Component {
     Meteor.call('tasks.remove', this.props.task._id);
   }
 
+  togglePrivate() {
+    Meteor.call('tasks.setPrivate', this.props.task._id, ! this.props.task.private);
+  }
+
   render() {
 
     const taskClassName = this.props.task.checked ? 'checked' : '';
@@ -30,6 +34,11 @@ export default class Task extends Component {
           onClick={this.toggleChecked.bind(this)}
         />
 
+        { this.props.showPrivateButton ? (
+          <button className="toggle-private" onClick={this.togglePrivate.bind(this)}>
+            { this.props.task.private ? 'Private' : 'Public' }
+          </button>
+        ) : ''}
 
         <span className="text">
           <strong>{this.props.task.username}</strong>: {this.props.task.text}
@@ -38,3 +47,8 @@ export default class Task extends Component {
     );
   }
 }
+
+Task.propTypes = {
+  task: PropTypes.object.isRequired,
+  showPrivateButton: React.PropTypes.bool.isRequired,
+};
